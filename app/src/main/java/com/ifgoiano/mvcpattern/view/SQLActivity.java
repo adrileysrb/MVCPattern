10f411cb43d6971dcfa0e1e92e4b97bc7f8a2874
Adriley Samuel Ribeiro Barbosa <asbarbosa@stefanini.com>
2022-01-31 18:36:23 -0300

    Improvements

--- app/src/main/java/com/ifgoiano/mvcpattern/view/SQLActivity.java
+++ app/src/main/java/com/ifgoiano/mvcpattern/view/SQLActivity.java
@@ -37,12 +37,7 @@ public class SQLActivity extends AppCompatActivity {
         setTitle("MVC Activity - LOCAL");
 
         list = findViewById(R.id.list_sql);
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(SQLActivity.this, "ID: "+listValuesIDs.get(position)+ " - "+listValues.get(position), Toast.LENGTH_SHORT).show();
-            }
-        });
+        list.setOnItemClickListener((parent, view, position, id) -> Toast.makeText(SQLActivity.this, "ID: "+listValuesIDs.get(position)+ " - "+listValues.get(position), Toast.LENGTH_SHORT).show());
 
 
         CountriesLocalRepositoryController =new CountriesLocalRepositoryController(SQLActivity.this);
@@ -50,37 +45,15 @@ public class SQLActivity extends AppCompatActivity {
         Button insert=findViewById(R.id.insert_data);
         Button update=findViewById(R.id.update_data);
         Button read=findViewById(R.id.refresh_data);
-       // datalist=findViewById(R.id.all_data_list);
         datalist_count=findViewById(R.id.data_list_count);
 
-        read.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                refreshData();
+        read.setOnClickListener(v -> refreshData());
 
-            }
-        });
+        insert.setOnClickListener(v -> ShowInputDialog());
 
-        insert.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ShowInputDialog();
-            }
-        });
+        update.setOnClickListener(v -> showUpdateIdDialog());
 
-        update.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showUpdateIdDialog();
-            }
-        });
-
-        delete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showDeleteDialog();
-            }
-        });
+        delete.setOnClickListener(v -> showDeleteDialog());
 
     }
 
@@ -88,12 +61,8 @@ public class SQLActivity extends AppCompatActivity {
         datalist_count.setText("ALL DATA COUNT : " + CountriesLocalRepositoryController.getToltalRegistersCount());
 
         List<Country> studentModelList= CountriesLocalRepositoryController.getAllCountries();
-//        datalist.setText("");
         listValues = new ArrayList<>();
         for(Country studentModel:studentModelList){
-            //"ID : "+studentModel.getId()+" | Name : "+studentModel.getCountryName()+" \n\n"
-            //datalist.append("DB ID: "+studentModel.getId()+" - Country: "+studentModel.getCountryName()+" \n\n");
-            //datalist.setPadding(30, 30, 30, 30);
             listValues.add(studentModel.getCountryName());
             listValuesIDs.add(studentModel.getId());
         }
@@ -109,14 +78,11 @@ public class SQLActivity extends AppCompatActivity {
         Button delete_btn=view.findViewById(R.id.delete_btn);
         final AlertDialog alertDialog=al.show();
 
-        delete_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CountriesLocalRepositoryController.deleteCountry(id_input.getText().toString());
-                alertDialog.dismiss();
-                refreshData();
+        delete_btn.setOnClickListener(v -> {
+            CountriesLocalRepositoryController.deleteCountry(id_input.getText().toString());
+            alertDialog.dismiss();
+            refreshData();
 
-            }
         });
 
 
@@ -129,13 +95,10 @@ public class SQLActivity extends AppCompatActivity {
         final EditText id_input=view.findViewById(R.id.id_input);
         Button fetch_btn=view.findViewById(R.id.update_id_btn);
         final AlertDialog alertDialog=al.show();
-        fetch_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showDataDialog(id_input.getText().toString());
-                alertDialog.dismiss();
-                refreshData();
-            }
+        fetch_btn.setOnClickListener(v -> {
+            showDataDialog(id_input.getText().toString());
+            alertDialog.dismiss();
+            refreshData();
         });
 
     }
@@ -151,16 +114,13 @@ public class SQLActivity extends AppCompatActivity {
         name.setText(studentModel.getCountryName());
 
         final AlertDialog alertDialog=al.show();
-        update_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Country studentModel=new Country();
-                studentModel.setCountryName(name.getText().toString());
-                studentModel.setId(id);
-                CountriesLocalRepositoryController.updateCountry(studentModel);
-                alertDialog.dismiss();
-                refreshData();
-            }
+        update_btn.setOnClickListener(v -> {
+            Country studentModel1 =new Country();
+            studentModel1.setCountryName(name.getText().toString());
+            studentModel1.setId(id);
+            CountriesLocalRepositoryController.updateCountry(studentModel1);
+            alertDialog.dismiss();
+            refreshData();
         });
     }
 
@@ -173,15 +133,12 @@ public class SQLActivity extends AppCompatActivity {
 
         final AlertDialog alertDialog=al.show();
 
-        insertBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Country studentModel=new Country();
-                studentModel.setCountryName(name.getText().toString());
-                CountriesLocalRepositoryController.addCountry(studentModel);
-                alertDialog.dismiss();
-                refreshData();
-            }
+        insertBtn.setOnClickListener(v -> {
+            Country studentModel=new Country();
+            studentModel.setCountryName(name.getText().toString());
+            CountriesLocalRepositoryController.addCountry(studentModel);
+            alertDialog.dismiss();
+            refreshData();
         });
     }
 }
